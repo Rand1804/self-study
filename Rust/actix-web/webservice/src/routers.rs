ff9b869d2ef0fcbfbb13f0674e617a9853512b40
wwt <wuwentao.c@gmail.com>
2023-05-11 23:09:34 +0800

    actix-web

---
+++ Rust/actix-web/webservice/src/routers.rs
@@ -0,0 +1,11 @@
+use super::handlers::*;
+use actix_web::web;
+
+pub fn general_routes(cfg: &mut web::ServiceConfig) {
+    cfg.route("/health", web::get().to(health_check_handler));
+}
+
+pub fn course_routers(cfg: &mut web::ServiceConfig) {
+    cfg.service(web::scope("/courses"))
+        .route("/", web::post().to(new_course));
+}
